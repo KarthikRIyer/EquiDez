065aae2b4199b79dcfe295a431599ef628aa4306
kaustubhmaheshwari <hi2bi2ki2@gmail.com>
2019-03-10 02:35:44 +0700

    updated shell and head

--- src/HeadDesign.java
+++ src/HeadDesign.java
@@ -21,12 +21,30 @@ class HeadDesign{
                 C=0.55;
                 break;
         }
-        t= C*(prop.getDi())* Math.sqrt(prop.getDesignP()/prop.getf());
+        t= C*(prop.getDi())* Math.sqrt(prop.getDesignP()/prop.getF());
         return t;
     }
     double conicalHead(){
-        double t1,t2,Z;
-        double arrayZ[]={};
-        
+        double t1,t2,Z,l,Dk;
+        switch(prop.getAlpha()) {
+            case 20:
+                Z=1.00;
+                break;
+            case 30:
+                Z=1.35;
+                break;
+            case 45:
+                Z=2.05;
+                break;
+            case 60:
+                Z=3.2;
+                break;
+        }
+        t1= ((prop.getDesignP())*(prop.getDo())*Z/((2*prop.getF()*prop.getJ())))/1000;
+        l= 0.5* Math.sqrt(1000*(prop.getDo()*prop.getTStd())/Math.cos(Math.toRadians(prop.getAlpha())));
+        Dk= prop.getDi()-(2*l*math.sin(Math.toRadians(prop.getAlpha())));
+        t2= (prop.getDesignP()*Dk)/(((2*prop.getF()*prop.getJ())-prop.getDesignP())*Math.cos(Math.toradians(prop.getAlpha()))*1000);
+
+
     }
 }
