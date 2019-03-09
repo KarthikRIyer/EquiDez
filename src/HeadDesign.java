eb548d275b0a7cc085bfa502bcc0999335d303dc
kaustubhmaheshwari <hi2bi2ki2@gmail.com>
2019-03-10 03:49:48 +0700

    updated head

--- src/HeadDesign.java
+++ src/HeadDesign.java
@@ -29,7 +29,7 @@ class HeadDesign {
     }
 
     double conicalHead() {
-        double t1, t2, Z, l, Dk,t1std,t2std;
+        double t1, t2, Z, l, Dk, t1std, t2std;
         switch (prop.getAlpha()) {
             case 20:
                 Z = 1.00;
@@ -48,13 +48,31 @@ class HeadDesign {
         l = 0.5 * Math.sqrt(1000 * (prop.getDo() * prop.getTStd()) / Math.cos(Math.toRadians(prop.getAlpha())));
         Dk = ((1000 * prop.getDi()) - (2 * l * math.sin(Math.toRadians(prop.getAlpha())))) / 1000;
         t2 = (prop.getDesignP() * Dk) / (((2 * prop.getF() * prop.getJ()) - prop.getDesignP()) * Math.cos(Math.toradians(prop.getAlpha())) * 1000);
-        t1std=StandardThickness.standardT(t1,prop);
-        t2std=StandardThickness.standardT(t2,prop);
-        if(t1>t2){
-            t=t1std;
+        t1std = StandardThickness.standardT(t1, prop);
+        t2std = StandardThickness.standardT(t2, prop);
+        if (t1 > t2) {
+            t = t1std;
+        } else {
+            t = t2std;
         }
-        else{
-            t=t2std;
+    }
+
+    double torisphericalHead() {
+        double ri, Ri, trand, ro, Ro, param1, param2, param3, he, C;//param1=ho,param2=Do2/4*Ro,param3=sqrt(Do*ro/2)
+        trand = prop.getTStd();
+        ri = 0.06 * prop.getDi();
+        Ri = 0.64 * prop.getDi();
+        for (int i = 0; i < 3; i++) {
+            ro = ri + 2 * trand;
+            Ro = Ri + 2 * trand;
+            param1 = Ro - Math.sqrt(((Ro - (prop.getDo() / 2)) * (Ro + ((prop.getDo()) / 2) - (2 * ro))));
+            param2 = (Math.pow((prop.getDo()), 2)) / (2 * Ro);
+            param3 = sqrt((prop.getDo()) * ro / 2);
+            he = Math.min(Math.min(param1, param2), param3);
+            C = prop.getC((he / prop.getDo()), (trand / prop.getDo()));
+            trand = ((prop.getDesignP()) * (prop.getDo()) * C) / ((2 * (prop.getF())) * prop.getJ());
         }
+        t = StandardThickness.standardT(trand, prop);
+
     }
 }
