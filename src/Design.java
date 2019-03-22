6d9095227aedf57de985f8c4e29237bf9bf208d9
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-03-23 01:04:45 +0530

    fixes

--- src/Design.java
+++ src/Design.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class Design {
 
-    HeadDesign headDesign;
-    NozzleCompensator nozzleCompensator;
-    Properties prop;
-    ShellDesigner shellDesigner;
-
     public static void main(String args[]){
+
+        HeadDesign headDesign;
+        NozzleCompensator nozzleCompensator;
+        Properties prop = new Properties();
+        ShellDesigner shellDesigner;
+
         Scanner in=new Scanner(System.in);
 
         System.out.println("Enter your working Pressure.(in kPa)");
@@ -19,7 +21,8 @@ public class Design {
         System.out.println("Which type of Shell do you want to design?");
         System.out.println("1.     Cylindrical Shell (type: 1)");
         System.out.println("2.     Spherical Shell   (type: 2)");
-        shellDesigner.shellType=in.nextDouble();
+        shellDesigner = new ShellDesigner(prop, 1);
+        double shellType = in.nextDouble();
         shellDesigner.getTr();
         shellDesigner.standardt();
         
