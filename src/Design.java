15b30f5daf7f0a6de4e71ba62e3ac66f219760c4
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-03-22 23:57:29 +0530

    added main function

---
+++ src/Design.java
@@ -0,0 +1,14 @@
+public class Design {
+
+    HeadDesign headDesign;
+    NozzleCompensator nozzleCompensator;
+    Properties properties;
+    ShellDesigner shellDesigner;
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World");
+
+    }
+
+}
